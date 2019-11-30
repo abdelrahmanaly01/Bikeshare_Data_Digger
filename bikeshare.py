8617fed2ea02528ed8e58810bc3d40c495f758c9
Bido_Boy(69) <abdosad251@yahoo.com>
2019-11-30 09:41:41 -0500

    shorten the name of duration function

--- bikeshare.py
+++ bikeshare.py
@@ -107,7 +107,7 @@ def station_stats(df):
     print('-'*40)
 
 
-def trip_duration_stats(df):
+def duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
@@ -181,7 +181,7 @@ def main():
 
         time_stats(df)
         counter = 0
-        info_list = ['station_stats(df)', 'trip_duration_stats(df)', 'user_stats(df)']
+        info_list = ['station_stats(df)', 'duration_stats(df)', 'user_stats(df)']
         info_name = ["Stations ","Trip Duration","users"]
         while counter <= 2:
             answer=input("would you like to show information about {} ?('yes' or 'skip') : ".format(info_name[counter])).lower()
